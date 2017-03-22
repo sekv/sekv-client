32b9b1ea851868ace3d1880923b884641886bc7d
Yuxia <rainycheng1988@163.com>
2017-03-22 16:21:00 +1100

    append operation ok

--- libsekv_op.c
+++ libsekv_op.c
@@ -6,6 +6,8 @@
 #include "encrypt.h"
 #include "hash_op.h"
 
+int mcounter;
+
 int sekv_connect_server()
 {
     int client_sockfd;
@@ -202,7 +204,91 @@ int sekv_get(int sockfd, char *key, int flags, int exptime, int bytes, char *val
 
 int sekv_append(int sockfd, char *key, int flags, int exptime, int bytes, char *value)
 {
-   
+    char buf[BUFSIZ], comm[BUFSIZ], bufv[BUFSIZ],sb[BUFSIZ];
+    // <command name> <key> <flags> <exptime> <bytes> [noreply]\r\n
+    memset(buf,0,BUFSIZ);memset(comm,0,BUFSIZ);
+    memset(bufv,0,BUFSIZ);memset(sb,0,BUFSIZ);
+    char *command="append ";
+    strcpy(comm, command);
+    // encrypt <key>
+    unsigned char *p_enc, *p_mac;
+    int len_dst;
+    p_enc = (unsigned char *)malloc(sizeof(unsigned char)*strlen(key));
+    p_mac = (unsigned char *)malloc(sizeof(unsigned char)*16);
+    my_aes_gcm_encrypt(key,strlen(key),p_enc,&len_dst,p_mac);
+    printf("strlen(p_enc):%d\n",strlen(p_enc));
+    printf("strlen(p_mac):%d\n",strlen(p_mac));
+    // the return MAC length may vary, we use the first 16 bytes
+    strncat(comm,p_mac,16);
+    strcat(comm,p_enc);
+    // the value length bytes+16, 16 is the length of MAC, 20 bytes of meta sha1, 4 bytes of version number,8 bytes of command name, 16 bytes of update number
+    sprintf(buf," %d %d %d",flags,exptime,bytes+16+20+4+8+16);
+    strcat(comm,buf);
+    strcat(comm,"\r\n");
+    printf("command:\n");
+    BIO_dump_fp(stdout,comm,strlen(comm));
+    int len;
+    // send set command
+    len=send(sockfd,comm,strlen(comm),0);
+
+    // version tracking table operations
+    item *it_new, *it;
+    uint32_t hv;
+//    it_new = (item *)malloc(sizeof(item));
+//    it_new->key = (char *)malloc(sizeof(char)*16);
+//    it_new->nkey = 16;
+//    it_new->vn = 0;
+//    strncpy(it_new->key,p_mac,16);
+    hv = MurmurHash3_x86_32(p_mac, 16);
+    it = assoc_find(p_mac, 16, hv);
+    if(it){
+//        assoc_delete(it_new->key,it_new->nkey, hv);
+        printf("append command: find the same key!\n");
+    }else{
+        printf("append command: not find the same key!\n");
+    }
+//    assoc_insert(it_new, hv);
+
+    free(p_enc);
+    free(p_mac);
+    // encrypt <MACmeta,vn,value>
+    p_enc = (unsigned char *)malloc(sizeof(unsigned char)*(strlen(value)+20+4+8+16));
+    p_mac = (unsigned char *)malloc(sizeof(unsigned char)*16);
+    // MACmeta is the sha1 message digest
+    unsigned char *md_value;
+    int md_len;
+    md_value = (unsigned char *)malloc(sizeof(unsigned char)*20);
+    sprintf(buf,"%s %d %d",key,flags,exptime);
+    printf("meta:%s\n",buf);
+    my_sha1(buf,md_value,&md_len);
+    memset(bufv,0,BUFSIZ);
+    strncpy(bufv,md_value,20);
+    // 4 bytes long version number, initial value 0
+    sprintf(buf,"%4d",0);
+    strncat(bufv,buf,4);
+    sprintf(buf,"%8s","append");
+    strncat(bufv,buf,8);
+    mcounter++;
+    sprintf(buf,"%16d",mcounter);
+    strncat(bufv,buf,16);
+    strcat(bufv,value);
+    printf("bufv:%s\n",bufv);
+    printf("strlen(bufv):%d\n",strlen(bufv));
+    my_aes_gcm_encrypt(bufv,strlen(bufv),p_enc,&len_dst,p_mac);
+
+    memset(sb,0,BUFSIZ);
+    strncpy(sb,p_mac,16);
+    strcat(sb,p_enc);
+    strcat(sb,"\r\n");
+    printf("value:\n");
+    BIO_dump_fp(stdout,sb,strlen(sb));
+    printf("strlen(bufv):%d\n",strlen(sb));
+    // send encrypted <value>
+    len=send(sockfd,sb,strlen(sb),0);
+    memset(buf,0,BUFSIZ);
+    len=recv(sockfd,buf,BUFSIZ,0);
+    printf("%s\n",buf);
+    return 0;   
 }
 
 int main()
@@ -211,10 +297,13 @@ int main()
    char *value="hello world! My first runnable SeKV get set operations";
    int re=0;
    int sockfd;
+   mcounter=0;
    assoc_init(16);
    sockfd = sekv_connect_server();
    re = sekv_set(sockfd,key,0,0,strlen(value),value);
    re = sekv_get(sockfd,key,0,0,0,NULL);
    re = sekv_set(sockfd,key,0,0,strlen(value),value);
    re = sekv_get(sockfd,key,0,0,0,NULL);
+   re = sekv_append(sockfd,key,0,0,10,"haha124abc");
+   re = sekv_append(sockfd,key,0,0,7,"haha124");  
 } 
